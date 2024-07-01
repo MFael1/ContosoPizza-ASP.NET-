036dbe961136c212c3c3da5f9a98cb9d8115c9e1
FAEL <muhmmdfa624@gmail.com>
2024-07-01 11:52:44 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using ContosoPizza.Data;
+using ContosoPizza.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddEntityFrameworkNpgsql().
+AddDbContext<PizzaContext>(opt =>
+opt.UseNpgsql(builder.Configuration.GetConnectionString("SampleConnectionString")));
+// Add the PromotionsContext
+
+builder.Services.AddScoped<PizzaService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Add the CreateDbIfNotExists method call
+app.CreateDbIfNotExists();
+
+app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
+
+app.Run();
